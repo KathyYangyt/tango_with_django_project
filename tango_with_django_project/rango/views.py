22a6a52a3c490eab21ff279848da6c73ac6fd0a5
YitingYang <2538069Y@student.ac.uk>
2022-02-15 13:26:12 +0000

    chapter3

---
+++ tango_with_django_project/rango/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def index(request):
+        return HttpResponse("<a href='/rango/about/'>About</a>")
+
+def about(request):
+        return HttpResponse("<a href='/rango/index'>Index</a>")
