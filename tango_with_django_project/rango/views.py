39213cbb2e02c7560bed1d607f89a005e70898f6
YitingYang <2538069Y@student.ac.uk>
2022-02-23 00:11:20 +0000

    chapter 3 (with test)

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,58 +1,9 @@
-from django.shortcuts import render, redirect
-
-from rango.forms import CategoryForm
-from rango.models import Category
-from rango.models import Page
+from django.http import HttpResponse
 
 
 def index(request):
-    # context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-    category_list = Category.objects.order_by('-likes')[:5]
-    page_list = Page.objects.order_by('-views')[:5]
-
-    context_dict = {}
-    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
-    context_dict['categories'] = category_list
-    context_dict['pages'] = page_list
-
-    return render(request, 'rango/index.html', context=context_dict)
-
-
-def add_category(request):
-    form = CategoryForm()
-
-    if request.method == 'POST':
-        form = CategoryForm(request.POST)
-
-        if form.is_valid():
-            cat = form.save(commit=True)
-            #print(cat, cat.slug)
-            return redirect('/rango/')
-        else:
-            print(form.errors)
-
-
-    return render(request, 'rango/add_category.html', {'form': form})
+    return HttpResponse("Rango says hey there partner! <a href='/rango/about'>About</a>")
 
 
 def about(request):
-    context_dict = {'boldmessage': 'This tutorial has been put together by Yiting'}
-    return render(request, 'rango/about.html', context=context_dict)
-    # return HttpResponse("Rango says here is the about page.<a href='/rango/index/'>Index</a> ")
-
-
-def show_category(request, category_name_slug):
-    context_dict = {}
-
-    try:
-        category = Category.objects.get(slug=category_name_slug)
-
-        pages = Page.objects.filter(category=category)
-
-        context_dict['pages'] = pages
-        context_dict['category'] = category
-    except Category.DoesNotExist:
-        context_dict['category'] = None
-        context_dict['pages'] = None
-
-    return render(request, 'rango/category.html', context=context_dict)
+    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
