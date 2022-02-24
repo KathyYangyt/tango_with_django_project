541f14c89a0e7f3329aa45a621676f660578d907
YitingYang <2538069Y@student.ac.uk>
2022-02-24 00:02:07 +0000

    chapter 9 (tested)

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.template.defaultfilters import slugify
 
@@ -30,6 +31,15 @@ class Page(models.Model):
     url = models.URLField()
     views = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.title
 
-def __str__(self):
-    return self.title
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
